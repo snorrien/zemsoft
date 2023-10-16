eadd139f08cc3782eeb725385486b4227a019f76
Alena Shapovalova <avshapovalova31@gmail.com>
2023-10-16 13:01:52 +0200

    feat: styles to dropdown

--- vue-project/src/views/Contact.vue
+++ vue-project/src/views/Contact.vue
@@ -11,6 +11,7 @@ export default {
     data() {
         console.log(this.contactName)
         const contact = this.$store.state.contacts.find(c => c.name === this.contactName);
+
         return {
             id: contact ? contact.id : '',
             name: contact ? contact.name : '',
@@ -19,7 +20,12 @@ export default {
             group: contact ? contact.group : '',
             date: contact ? contact.date : null,
             nameError: null,
-            emailError: null
+            emailError: null,
+            phoneError: null,
+
+            valueDropdown: 'ВСЕ',
+            list: ["ВСЕ", "Родственники", "Коллеги"],
+            visible: false,
         }
     },
     methods: {
@@ -66,8 +72,9 @@ export default {
         validateInputs() {
             this.validateName()
             this.validateEmail()
+            this.validatePhone()
 
-            if (this.nameError || this.emailError) {
+            if (this.nameError || this.emailError || this.phoneError) {
                 return false;
             } else {
                 return true;
@@ -75,14 +82,23 @@ export default {
         },
         validateName() {
             const value = this.name
-            if (value.length <= 3) {
+            if (value.length < 3) {
                 this.nameError = 'Слишком короткое имя'
             } else if (value.length === 0) {
                 this.nameError = 'Поле не можеть быть пустым'
             } else {
                 this.nameError = null;
             }
-
+        },
+        validatePhone() {
+            const value = this.phone;
+            if (value.length < 17) {
+                this.phoneError = 'Некорректный номер'
+            } else if (value.length === 0) {
+                this.phoneError = 'Поле не можеть быть пустым'
+            } else {
+                this.phoneError = null;
+            }
         },
         validateEmail() {
             const value = this.email;
@@ -91,10 +107,17 @@ export default {
                 this.emailError = null
             } else if (value.length === 0) {
                 this.emailError = 'Поле не можеть быть пустым'
-            }else {
-                this.emailError = "Некорректный e-mail"
+            } else {
+                this.emailError = 'Некорректный e-mail'
             }
         },
+        toggleDropdown() {
+            this.visible = !this.visible;
+        },
+        selectItem(option) {
+            this.valueDropdown = option;
+
+        }
     }
 }
 </script>
@@ -117,13 +140,16 @@ export default {
             <div class="container_input">
                 <input v-model="name" class="input" type="text" placeholder="Например «Андрей»...">
                 <img :class="{ active: nameError === null }" src="../imgs/invalid-icon.svg" />
-                <span v-if="nameError">{{ nameError }}</span>
+                <span class="error-text" v-if="nameError">{{ nameError }}</span>
             </div>
         </div>
         <div class="container_row">
             <p class="container_label">Телефон</p>
             <div class="container_input">
-                <input v-model="phone" placeholder="+7 (___) ___ __ __" class="input"  v-maska data-maska="+7(###)-###-##-##"/>
+                <input v-model="phone" placeholder="+7 (___) ___ __ __" class="input" v-maska
+                    data-maska="+7(###)-###-##-##" />
+                <img :class="{ active: phoneError === null }" src="../imgs/invalid-icon.svg" />
+                <span class="error-text" v-if="phoneError">{{ phoneError }}</span>
             </div>
         </div>
         <div class="container_row">
@@ -131,19 +157,27 @@ export default {
             <div class="container_input">
                 <input v-model="email" class="input" placeholder="Например «pochta@domain.ru»...">
                 <img :class="{ active: emailError === null }" src="../imgs/invalid-icon.svg" />
-                <span v-if="emailError">{{ emailError }}</span>
+                <span class="error-text" v-if="emailError">{{ emailError }}</span>
             </div>
         </div>
         <div class="container_row">
 
             <p class="container_label">Категория</p>
-            <div>
-                <div class="container_input">
-                    <select v-model="group" class="input">
-                        <option>ВСЕ</option>
-                        <option>Родственники</option>
-                        <option>Коллеги</option>
-                    </select>
+            <div class="container_input">
+                <div class="input aselect" :data-value="valueDropdown" :data-list="list">
+                    <div class="selector" @click="toggleDropdown()">
+                        <div class="label">
+                            {{ valueDropdown }}
+                        </div>
+                        <div class="arrow" :class="{ expanded: visible }"></div>
+                        <div :class="{ hidden: !visible, visible }">
+                            <ul>
+                                <li :class="{ current: item === valueDropdown }" v-for="item in list"
+                                    @click="selectItem(item)">{{ item }}
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -174,6 +208,7 @@ export default {
 </template>
 
 <style lang="scss">
+
 .contact_title {
     background-color: var(--bg-black);
     color: var(--title-text);
@@ -183,7 +218,6 @@ export default {
     display: flex;
     justify-content: end;
     align-items: center;
-
 }
 
 .contact_title__name {
@@ -242,7 +276,7 @@ export default {
     margin-left: 40px;
 }
 
-.container_input span {
+.error-text {
     position: absolute;
     right: 0;
     top: 100%;
@@ -268,6 +302,82 @@ export default {
 
 ::placeholder {
     color: #A9A9A9;
+}
+
+.aselect {
+    position: relative;
+    cursor: pointer;
+
+    .selector {
+        z-index: 1;
+
+        .arrow {
+            position: absolute;
+            right: 12px;
+            top: 40%;
+            width: 0;
+            height: 0;
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+            border-top: 5px solid rgba(169, 169, 169, 1);
+            transform: rotateZ(0deg) translateY(0px);
+            transition-duration: 0.3s;
+            transition-timing-function: cubic-bezier(.59, 1.39, .37, 1.01);
+        }
+
+        .expanded {
+            transform: rotateZ(180deg) translateY(2px);
+        }
+
+        .label {
+            line-height: normal;
+        }
+
+        .label  {
+            font-weight: 700;
+            &:hover{
+                font-weight: 400;
+            }
+        }
+    }
+
+    ul {
+        width: 100%;
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        background-color: var(--bg-white);
+        position: absolute;
+        left: -2px;
+        top: 42px;
+        z-index: 1;
+        border-radius: 4px;
+        box-shadow: 0px 0px 6px 0px rgba(148, 181, 225, 0.35);
+    }
+
+    li {
+        padding: 10px 8px 10px 16px;
+
+        &:hover {
+            background: var(--bg-l-blue);
+            font-weight: 400;
+        }
+    }
+
+    .current {
+        background-image: url("../imgs/choose-yes.svg");
+        background-repeat: no-repeat;
+        background-position: calc(100% - 8px) center;
+        font-weight: 700;
+    }
+
+    .hidden {
+        visibility: hidden;
+    }
+
+    .visible {
+        visibility: visible;
+    }
 }
 
 .container_buttons {
@@ -299,7 +409,7 @@ export default {
     }
 }
 
-.active{
+.active {
     display: none;
 }
 
